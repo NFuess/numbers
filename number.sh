584f51ca99f3222130341da325e1741189e64a22
Nick Fuess <fuess@chapman.edu>
2019-11-19 13:55:08 -0800

    Adds new version of number.sh

--- number.sh
+++ number.sh
@@ -9,9 +9,9 @@ do
 	if [ $((number%2)) -eq 0 ]
 	then
 		echo "$number is even"
-		number=$[$number+1]
+		number=$((number+1))
 	else
 		echo "$number is odd"
-		number=$[$number+1]
+		number=$((number+1))
 	fi
 done
